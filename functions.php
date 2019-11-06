5c651e68a7e2c918d79c877f8550e4e83591d2eb
Surya <hendricks.surya@gmail.com>
2019-11-06 09:25:29 +0100

    done

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ function sendmailtoclient($clmsg, $m)
 
 
         $mail->send();
-        // echo 'Message has been sent';
+        echo 'Message has been sent';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
